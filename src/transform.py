8f6098f615c726b51c6ab5bd73ce2b2384602a82
Vikram Grewal <grewav2@student.ubc.ca>
2024-02-08 13:15:33 -0500

    added docstrings

--- src/transform.py
+++ src/transform.py
@@ -4,6 +4,19 @@ import numpy as np
 class transform():
 
     def __init__(self):
+        """Class used to transform images from front to bird's-eye views.
+        
+        Attributes
+        ----------
+        src : array-like
+            The ROI points on the front-view image.
+        dst : array-like
+            The destination points for the bird's-eye view.
+        matrix : array-like
+            Matrix to transform from front to bird's-eye.
+        matrix_inv : array-like
+            Matrix to transform from bird's-eye to front. 
+        """
         self.src = np.int32([
             (550, 450),
             (250, 650),
@@ -22,10 +35,35 @@ class transform():
         self.matrix_inv = cv2.getPerspectiveTransform(self.dst, self.src)
 
     def orig_to_bev(self, img):
+        """Transforms an image to bird's-eye view.
+
+        Only a pre-specified region of the image will be transformed and returned.
+
+        Parameters
+        ----------
+        img : array-like
+            The image to be transformed.
+
+        Returns
+        -------
+        array-like
+            The bird's-eye view of the region.
+        """
         result = cv2.warpPerspective(img, self.matrix, (img.shape[1], img.shape[0]))
         return result
     
     def bev_to_orig(self, img):
+        """Transforms a bird's-eye view to a front-view.
+
+        Parameters
+        ----------
+        img : array-like
+            The transformed bird's-eye view.
+
+        Returns
+        -------
+        array-like
+            The transformed front-view.
+        """
         result = cv2.warpPerspective(img, self.matrix_inv, (img.shape[1], img.shape[0]))
         return result
-
