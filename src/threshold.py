e1b5772f14e993b590c2698b5839332917ad062c
Vikram Grewal <grewav2@student.ubc.ca>
2024-02-12 13:13:41 -0500

    created thresholding script

---
+++ src/threshold.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+class threshold():
+    def __init__(self):
+        pass
+
+    def threshold(img):
+        thresh = (200,255)
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        v_channel = hsv[:, :, 2]
+
+        output = np.zeros_like(v_channel)
+        output[(v_channel >= thresh[0]) & (v_channel <= thresh[1])] = 1
+
+        return output
+
