33f40326be70527f28827a7be1fadbf488655352
Vikram Grewal <grewav2@student.ubc.ca>
2024-02-13 13:17:15 -0500

    added hue channel thresholding

--- src/threshold.py
+++ src/threshold.py
@@ -5,13 +5,15 @@ class Threshold():
     def __init__(self):
         pass
 
-    def threshold(img):
-        thresh = (200,255)
+    def apply_threshold(self, img, v_thresh, h_thresh):
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h_channel = hsv[:, :, 0]
         v_channel = hsv[:, :, 2]
 
-        output = np.zeros_like(v_channel)
-        output[(v_channel >= thresh[0]) & (v_channel <= thresh[1])] = 1
+        h_img = np.zeros_like(v_channel)
+        h_img[(h_channel >= h_thresh[0]) & (h_channel <= h_thresh[1])] = 1
 
-        return output
+        v_img = np.zeros_like(v_channel)
+        v_img[(v_channel >= v_thresh[0]) & (v_channel <= v_thresh[1])] = 1
 
+        return h_img | v_img
