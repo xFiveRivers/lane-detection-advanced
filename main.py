1438ae1150971754f3afff7dcc01070b5f80cdbf
Vikram Grewal <grewav2@student.ubc.ca>
2024-02-13 17:03:18 -0500

    added video processing function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from moviepy.editor import VideoFileClip
 
 from src.calibration import *
 from src.transform import *
@@ -21,8 +22,11 @@ class LaneDetection:
         img = self.threshold.apply_threshold(img, (210, 255), (20, 30))
         img = self.lines.sliding_window(img)
         img = self.tranform.bev_to_orig(img)
-
         out_img = cv2.addWeighted(orig_img, 0.8, img, 1.0, 0.0)
 
+        return out_img
     
-        
+    def process_video(self, input_path, output_path):
+        input_clip = VideoFileClip(input_path)
+        output_clip = input_clip.fl_image(self.detect)
+        output_clip.write_videofile(output_path, audio=False)
