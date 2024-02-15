e9084611f4bf4393ae3e62fb14a0aaf9d2788faf
Vikram Grewal <grewav2@student.ubc.ca>
2024-02-15 11:52:35 -0500

    adjusted thresholding method

--- src/threshold.py
+++ src/threshold.py
@@ -6,16 +6,33 @@ class Threshold():
         pass
 
     def apply_threshold(self, img, v_thresh, h_thresh):
+        # Specify yellow threshold range
+        yellow_lower_range = np.array([0, 100, 100])
+        yellow_upper_range = np.array([50, 255, 255])
+
+        # Specify white threshold range
+        white_lower_range = np.array([215])
+        white_upper_range = np.array([255])
+
+        # Get colour spaces
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        h_channel = hsv[:, :, 0]
-        v_channel = hsv[:, :, 2]
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        h_img = np.zeros_like(h_channel)
-        h_img[(h_channel >= h_thresh[0]) & (h_channel <= h_thresh[1])] = 1
-
-        v_img = np.zeros_like(v_channel)
-        v_img[(v_channel >= v_thresh[0]) & (v_channel <= v_thresh[1])] = 1
-
-        output = v_img | h_img
+        # Apply colour-specific thresholding
+        yellow = cv2.inRange(hsv, yellow_lower_range, yellow_upper_range)
+        white = cv2.inRange(gray, white_lower_range, white_upper_range)
         
+        output = yellow | white
+
+        # h_channel = hsv[:, :, 0]
+        # v_channel = hsv[:, :, 2]
+
+        # h_img = np.zeros_like(h_channel)
+        # h_img[(h_channel >= h_thresh[0]) & (h_channel <= h_thresh[1])] = 1
+
+        # v_img = np.zeros_like(v_channel)
+        # v_img[(v_channel >= v_thresh[0]) & (v_channel <= v_thresh[1])] = 1
+
+        # output = v_img | h_img
+
         return output
