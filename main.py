b4c8d8653746ae87b1d4648acdac1517e2a944e7
Vikram Grewal <grewav2@student.ubc.ca>
2024-02-15 11:51:57 -0500

    added capability to draw sliding windows

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ from src.lines import *
 
 class LaneDetection:
 
-    def __init__(self):
+    def __init__(self, draw_boxes=False):
         self.calibration = Calibration('camera_cal', (9, 6))
         self.tranform = Transform()
         self.threshold = Threshold()
         self.lines = Lines()
+        self.draw_boxes = draw_boxes
 
     def detect(self, img):
         orig_img = np.copy(img)
         img = self.calibration.undistort(img)
         img = self.tranform.orig_to_bev(img)
         img = self.threshold.apply_threshold(img, (210, 255), (20, 30))
-        img = self.lines.sliding_window(img)
+        img = self.lines.sliding_window(img, self.draw_boxes)
         img = self.tranform.bev_to_orig(img)
         out_img = cv2.addWeighted(orig_img, 0.8, img, 1.0, 0.0)
 
@@ -45,6 +46,12 @@ class LaneDetection:
                 processed_frame = self.detect(frame)
                 # print(i)
                 out.write(processed_frame)
+
+                # if i == 6:
+                #     cv2.imwrite(f'output_media/debug/problem_frames/frame_{i}.png', frame)
+                # cv2.imwrite(f'output_media/debug/frame_{i}.png', np.concatenate((frame, processed_frame), axis=1))
+
+            
                 i += 1           
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
@@ -52,6 +59,7 @@ class LaneDetection:
                     break
             else:
                 break
+
         
         cap.release()
         out.release()
--- src/lines.py
+++ src/lines.py
@@ -5,9 +5,9 @@ class Lines():
 
     def __init__(self):
         self.img = None
-        self.margin = 50
+        self.margin = 100
         self.min_pixels = 50
-        self.n_windows = 6
+        self.n_windows = 9
         self.non_zero = None
         self.non_zero_x = None
         self.non_zero_y = None
@@ -15,6 +15,11 @@ class Lines():
         self.left_x_curr = None
         self.right_x_curr = None
         self.y_curr = None
+
+        self.top_left = None
+        self.bottom_right = None
+        self.out_img = None
+
 
 
     def draw_lines(self, left_x, left_y, right_x, right_y):
@@ -30,12 +35,12 @@ class Lines():
         draw_points_left = (np.asarray([draw_y_left, draw_x]).T).astype(np.int32)
         draw_points_right = (np.asarray([draw_y_right, draw_x]).T).astype(np.int32)
 
-        out_img = np.zeros((self.img.shape[0], self.img.shape[1], 3))
+        # out_img = np.zeros((self.img.shape[0], self.img.shape[1], 3))
 
-        cv2.polylines(out_img, [draw_points_left], False, (0, 255, 0), thickness=20)
-        cv2.polylines(out_img, [draw_points_right], False, (0, 255, 0), thickness=20)
+        cv2.polylines(self.out_img, [draw_points_left], False, (0, 255, 0), thickness=20)
+        cv2.polylines(self.out_img, [draw_points_right], False, (0, 255, 0), thickness=20)
         
-        return (out_img).astype(np.uint8)
+        return (self.out_img).astype(np.uint8)
 
 
     def get_features(self):
@@ -68,12 +73,12 @@ class Lines():
     def find_pixels(self, center):
     
         # Define top-left and bottom-right x and y coords for window boundry
-        top_left = (center[0] - self.margin, center[1] - (self.window_height // 2))
-        bottom_right = (center[0] + self.margin, center[1] + self.window_height // 2)
+        self.top_left = (center[0] - self.margin, center[1] - (self.window_height // 2))
+        self.bottom_right = (center[0] + self.margin, center[1] + self.window_height // 2)
         
         # Create conditions for array indexing
-        cond_x = (self.non_zero_x >= top_left[0]) & (self.non_zero_x <= bottom_right[0])
-        cond_y = (self.non_zero_y >= top_left[1]) & (self.non_zero_y <= bottom_right[1])
+        cond_x = (self.non_zero_x >= self.top_left[0]) & (self.non_zero_x <= self.bottom_right[0])
+        cond_y = (self.non_zero_y >= self.top_left[1]) & (self.non_zero_y <= self.bottom_right[1])
 
         # Index non-zero array to only take pixels within the window boundries
         targets_x = self.non_zero_x[cond_x & cond_y]
@@ -82,9 +87,10 @@ class Lines():
         return targets_x, targets_y
     
 
-    def sliding_window(self, img):
+    def sliding_window(self, img, draw_boxes=False):
 
         self.img = img
+        self.out_img = np.zeros((self.img.shape[0], self.img.shape[1], 3))
 
         # Get image parameters for sliding window
         self.get_features()
@@ -100,7 +106,12 @@ class Lines():
 
             # Get x and y coords of pixels that fall within the window
             left_window_x, left_window_y = self.find_pixels(center_left)
+            if draw_boxes == True:
+                cv2.rectangle(self.out_img, self.top_left, self.bottom_right, color=(255, 0, 0), thickness=10)
+
             right_window_x, right_window_y = self.find_pixels(center_right)
+            if draw_boxes == True:
+                cv2.rectangle(self.out_img, self.top_left, self.bottom_right, color=(255, 255, 0), thickness=10)
 
             # Add pixels detected in window to master lists
             left_x.extend(left_window_x)
