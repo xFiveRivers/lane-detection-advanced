efb41d685ac2fc3f129e2f432bb225371667ba90
Vikram Grewal <grewav2@student.ubc.ca>
2024-02-22 13:03:54 -0500

    added plot_channels function and docstrings

--- src/utility.py
+++ src/utility.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-def draw_roi(img, points, color):
+def draw_roi(img: np.ndarray, points: np.ndarray, color: tuple = (255, 0, 0)):
     """Draws the boundry of a region of interest on the image
 
     Parameters
@@ -28,3 +29,28 @@ def draw_roi(img, points, color):
     result = cv2.polylines(img, points, True, color, thickness=5)
 
     return result
+
+def plot_channels(dict: dict, title: str, nrows: int = 1, ncols: int = 3, figsize: tuple = (12, 3)):
+    """Plots the colour channels of a given colour space.
+
+    Parameters
+    ----------
+    dict : dict
+        A dictionary containing channel titles (keys) and arrays (values).
+    title : str
+        The title to display on the plot.
+    nrows : int, optional
+        Number of rows in the plot, by default 1.
+    ncols : int, optional
+        Number of columns (number of channels) in the plot, by default 3.
+    figsize : tuple, optional
+        Figure size in inches, by default (12, 3)
+    """
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+    axs = axs.flatten()
+    for channel, ax in zip(dict, axs):
+        ax.imshow(dict[channel], cmap='gray')
+        ax.title.set_text(channel)
+        ax.axis(False)
+    fig.suptitle(title)
+    plt.show()
